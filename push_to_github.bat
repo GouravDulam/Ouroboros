7419cd051e3c965d41ab16cd984edb6f5d78a950
GouravDulam <gouravdulam@gmail.com>
2026-05-02 14:07:22 +0530

    Update Netlify config and settings

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,8 +1,11 @@
 @echo off
+echo Fixing GitHub connection to point to your account (GouravDulam)...
+git remote set-url origin https://github.com/GouravDulam/Ouroboros.git
+
 echo Committing and pushing all recent changes to GitHub...
 git add .
 git commit -m "Update Netlify config and settings"
-git push
+git push -u origin main
 
 echo.
 echo Done! Netlify should start rebuilding automatically in a few seconds.
